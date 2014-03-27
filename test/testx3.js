8972d7e4232aef902d9be61e9ed7b6575d746119
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2014-03-27 13:53:17 +0900

    プロトタイプを使ったコンストラクタパターンを実装したのでコミット。

---
+++ test/testx3.js
@@ -0,0 +1,50 @@
+
+var PrototypeWithConstructor = function(name, age) {
+	this.name = name; // public スコープなプロパティ
+	this.age = age; // public スコープなプロパティ
+};
+PrototypeWithConstructor.prototype = {
+	toString: function() {
+		return this.name + ":" + this.age;
+	}
+};
+
+module("インスタンスの生成確認");
+test("コンストラクタで指定した値が取得できること", function() {
+	var sut = new PrototypeWithConstructor("hoge", 30);
+	strictEqual(sut.toString(), "hoge:30");
+
+});
+
+module("プロパティのスコープ確認");
+test("プロパティは public スコープなので外部から参照できることを確認する", function() {
+	var sut = new PrototypeWithConstructor("hoge", 30);
+	strictEqual(sut.toString(), "hoge:30", "コンストラクタで指定した値が取得できること");
+
+	strictEqual(sut.name, "hoge", "プロパティ hoge を参照できること");
+	strictEqual(sut.age, 30, "プロパティ age を参照できること");
+
+});
+
+test("プロパティは public スコープなので外部から値を変更できることを確認する", function() {
+	var sut = new PrototypeWithConstructor("hoge", 30);
+	strictEqual(sut.toString(), "hoge:30", "コンストラクタで指定した値が取得できること");
+
+	sut.name = "fuga";
+	sut.age = 40;
+	strictEqual(sut.toString(), "fuga:40", "コンストラクタで指定した値が変更されていること");
+
+});
+
+module("プロパティの非共有");
+test("プロパティの値がインスタンス間で共有されないことを確認する", function() {
+	var hoge30 = new PrototypeWithConstructor("hoge", 30);
+	strictEqual(hoge30.toString(), "hoge:30", "コンストラクタで指定した値が取得できること");
+
+	var fuga40 = new PrototypeWithConstructor("fuga", 40);
+	strictEqual(fuga40.toString(), "fuga:40", "コンストラクタで指定した値が取得できること");
+
+	fuga40.age = 41;
+	strictEqual(hoge30.toString(), "hoge:30", "fuga の age を変更しても hoge には反映されない");
+
+});
