931c4e91442273f0f1f4c43eb16a17dd5816b483
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2014-03-28 08:14:33 +0900

    コメントを修正したのでコミット。

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-
-module("");
-test("hoge", function() {
-	strictEqual("hoge", "hoge");
-});
-
-module("module pattern");
+/**
+ * モジュールパターンで定義したクラス。
+ *
+ * シングルトン？
+ */
 var ModulePattern = (function() {
 
-	var _name; // return で返さない → private スコープ
-	var _age; // return で返さない → private スコープ
-	var _gender = "male"; // return で返す → public スコープ
+	var _name; // スコープが private なプロパティ
+	var _age; // スコープが private なプロパティ
+	var _gender = "male"; // return で返す → スコープが public なプロパティ
 
-	// return で返すプロパティや関数はクラスの外部からアクセス可能 → public スコープ
+	/**
+	 * return で返すプロパティや関数はクラスの外部からアクセス可能 → public スコープ
+	 */
 	return {
-		gender: _gender,
+		gender: _gender, // "gender" という名前で外部からアクセスする
 
 		/**
 		 * プロパティを指定の値で初期化する。
@@ -40,28 +40,33 @@ var ModulePattern = (function() {
 		 */
 		setAge: function(age) {
 			_age = age;
-		}
+		},
 	};
+
 })();
+
+module("スコープが private なプロパティの初期化");
 test("疑似初期化関数を使ってプロパティの値を設定する", function() {
-	// コンストラクタ関数がないので、プロパティを初期化する疑似初期化関数で値を設定する
+	// コンストラクタ関数がないので、プロパティの初期化を行う「疑似初期化関数」で値を設定する
 	ModulePattern.init("hoge", 30);
 	strictEqual(ModulePattern.toString(), "hoge:30:male", "init 関数で設定した値が取得できること");
 
 });
-test("private スコープなプロパティの値は直接変更できないことを確認する", function() {
-	strictEqual(ModulePattern.toString(), "hoge:30:male", "テストケースが異なってもプロパティの値は同じであること");
 
-	ModulePattern._age = 31; // private スコープなプロパティの値は、クラスの外部から直接アクセスできない
-	strictEqual(ModulePattern.toString(), "hoge:30:male", "age の値は変わらないこと");
+module("プロパティのスコープ確認");
+test("スコープが private なプロパティの値は直接変更できないことを確認する", function() {
+	strictEqual(ModulePattern.toString(), "hoge:30:male", "以下で値を変更する前に格納されている値を確認");
 
-	ModulePattern.setAge(31); // setter メソッドを通せば private スコープであってもプロパティの値は変更できる
+	ModulePattern._age = 31; // スコープが private なプロパティの値は、クラスの外部から直接アクセスできない
+	strictEqual(ModulePattern.toString(), "hoge:30:male", "age の値は変わらないこと → 外部からアクセスできない");
+
+	ModulePattern.setAge(31); // setter メソッドを通せば、スコープが private なプロパティであっても値は変更できる
 	strictEqual(ModulePattern.toString(), "hoge:31:male", "age が 31 になること");
 
 });
-test("public スコープなプロパティの値は直接変更できることを確認する", function() {
-	ModulePattern.gender = "female"; // public スコープなプロパティの値は、クラスの外部から直接アクセスできる
+
+test("スコープが public なプロパティの値は直接変更できることを確認する", function() {
+	ModulePattern.gender = "female"; // スコープが public なプロパティの値は、クラスの外部から直接アクセスできる
 	strictEqual(ModulePattern.toString(), "hoge:31:female", "male が female になること");
 
 });
-
